d0e88714f54d0755f3be9a8369771086eb61ba39
David Labitzke <davidelabitzke@gmail.com>
2024-02-14 17:31:13 -0600

    Create main method

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ min_width = round(pyautogui.size().width - max_width)
 min_height = round(pyautogui.size().height - max_height)
 
 
-while True:
-    time.sleep(RESET_TIME)
-    new_x = randint(min_width, max_width)
-    new_y = randint(min_height, max_height)
-    pyautogui.moveTo(new_x, new_y, duration=RESET_TIME)
+def main():
+    while True:
+        time.sleep(RESET_TIME)
+        new_x = randint(min_width, max_width)
+        new_y = randint(min_height, max_height)
+        pyautogui.moveTo(new_x, new_y, duration=RESET_TIME)
+
+
+if __name__ == '__main__':
+    main()
